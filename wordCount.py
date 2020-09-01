4c2fdc404fc24cd0962254658f0dead699ee84b3
jamachado2 <jamachado2@miners.utep.edu>
2020-08-31 20:24:49 -0600

    Added comments in the file

--- wordCount.py
+++ wordCount.py
@@ -7,22 +7,28 @@ def wordCount():
 
         # Dictionary to store the total number of times each word appears
         wCounter = {}
-        # Reads the whole file and split the words
+        # Read the whole file and split the string into a list
         for line in d.read().split():
-                
-                #remove_punc = line.translate(str.maketrans('','', string.punctuation)).lower().split()
+                # ^ Matches the start of the string
+                # \w matches unicode word characters [a-zA-Z0-9_]
+                # \s matches unicode whitespace characters [\t\n\r\f\v]
+                # re.sub() return the string obtaied by replacing the leftmost non
+                # overlapping occurrences of pattern in string by the replacement repl
                 remove_punc = re.sub(r'[^\w\s]','',line).lower().split()
                 for word in remove_punc:
+                        # Add a word that is not in the dictionary
                         if word not in wCounter:
                             wCounter[word] = 1
+                        # Get the number of times the word appears in the .txt
                         else:
                             wCounter[word] += 1
-        
+
+        # Sort all words in the document in descending order
         for k,v in sorted(wCounter.items()):
                 output.write('{} {}'.format(k,v))
                 output.write('\n') 
                 #print('{:14} {:4}'.format(k,v))
 
-                
+# Runs everythong in the code                
 if __name__ == "__main__":
     wordCount()
